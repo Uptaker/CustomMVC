6790d5c40089765eee33f439358be348f31066e7
Uptaker <markus.tammeoja@gmail.com>
2021-08-30 18:20:02 +0300

    Add view loading functionality

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -9,11 +9,15 @@ class Pages extends Controller
 
     public function index()
     {
-        $this->view('hello');
+        $data = [
+            'title' => 'Welcome'
+        ];
+
+        $this->view('pages/index', $data);
     }
 
-    public function about($id)
+    public function about()
     {
-        echo "this is about " . " " . $id;
+        $this->view('pages/about');
     }
 }
